2e2b268d8192dd9fe2b848ce6d537e30da6f9bcf
mc303 <daniel@buonocore.nl>
2014-09-29 12:59:41 +0200

    From console application to Windows Application

--- createBG/modMain.vb
+++ createBG/modMain.vb
@@ -1,7 +1,114 @@
-﻿Module modMain
+﻿Imports Microsoft.Win32
+Imports System.Drawing
+Imports System.IO
+
+Module modMain
+    Const _locCorrect = 23
+
+    Public screenWidth As Integer = Screen.PrimaryScreen.Bounds.Width
+    Public screenHeight As Integer = Screen.PrimaryScreen.Bounds.Height
+
 
     Sub Main()
 
+
+        createPreviewFromBackground()
+
     End Sub
 
+    Private Sub loadItemsFromRegistryAddToForm()
+        Dim _inputFields As Integer = _reg.getInputFields()
+        _wallpaper = _reg.getRegWallpaper()
+
+        If Not _inputFields = 0 Then
+            For i As Integer = 0 To _inputFields - 1
+
+                With txt
+                    .Text = _reg.getItemText(i.ToString("D2"))
+                    .Name = "TextBox" & _nameN
+                    .ContextMenuStrip = cmsItems
+                    .ForeColor = ColorTranslator.FromWin32(_reg.getItemColor(i.ToString("D2")))
+                    .Size() = New System.Drawing.Size(_reg.getItemWidth(i.ToString("D2")), 21)
+                    .Font = _reg.getItemFont(i.ToString("D2"))
+                    .TextAlign = _reg.getItemAlign(i.ToString("D2"))
+                    '.Location = Convert.ToPointFromString(_reg.getItemLocation(i.ToString("D2")))
+                    .Location = PointToClient(_Convert.ToPointFromString(_reg.getItemLocation(i.ToString("D2"))))
+                    .BorderStyle = BorderStyle.FixedSingle
+                End With
+                Me.Controls.Add(txt)
+                Me.txt.BringToFront()
+                lbItems.Items.Add("TextBox" & _nameN)
+                _nameN = _nameN + 1
+            Next
+        End If
+
+        _inputFields = Nothing
+    End Sub
+
+    Sub createPreviewFromBackground()
+        Dim _txt As String
+        Dim i As Integer = 0
+        Dim _screenPos As Point
+        Dim _imgToMemoryStream As New MemoryStream()
+        Dim _color As Brush
+        Dim _envText As String
+        Dim stringFormat As New StringFormat()
+        Dim _inputFields As Integer = _reg.getInputFields()
+        Dim _wallpaper As String = _reg.getRegWallpaper()
+
+        'Load the Image to be written on.
+        Dim bitMapImage As Bitmap = Bitmap.FromFile(_wallpaper)
+        'Dim bitMapImage As Bitmap = Image.FromFile(_bg)
+        Dim graphicImage As Graphics = Graphics.FromImage(bitMapImage)
+
+        'Smooth graphics is nice.
+        'graphicImage.SmoothingMode = SmoothingMode.AntiAlias
+        graphicImage.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        graphicImage.TextRenderingHint = Drawing.Text.TextRenderingHint.ClearTypeGridFit
+        graphicImage.CompositingQuality = Drawing2D.CompositingQuality.HighQuality
+
+
+        If Not _inputFields = 0 Then
+            For i As Integer = 0 To _inputFields - 1
+                _txt = _reg.getItemText(i.ToString("D2"))
+                '_screenPos = _txt.PointToScreen(New Point(-21, -10))
+                _screenPos = _txt.PointToScreen(New Point(-5, 0))
+                'ScreenPos = _txt.Location
+                _color = New SolidBrush()
+                'Write your text.
+                Select Case _txt.TextAlign
+                    Case 0 'Left
+                        stringFormat.Alignment = StringAlignment.Near
+                        ' stringFormat.LineAlignment = StringAlignment.Near
+                    Case 1 'Right
+                        stringFormat.Alignment = StringAlignment.Far
+                        ' stringFormat.LineAlignment = StringAlignment.Far
+                        _screenPos = New Point(_screenPos.X + _txt.Width, _screenPos.Y)
+                    Case 2 'Center
+                        stringFormat.Alignment = StringAlignment.Center
+                        'stringFormat.LineAlignment = StringAlignment.Center
+                        _screenPos = New Point(_screenPos.X + (_txt.Width / 2), _screenPos.Y)
+                End Select
+
+                'TextRenderer.DrawText(graphicImage, _txt.Text, _txt.Font, New Point(ScreenPos), _txt.ForeColor, Color.Transparent, TextFormatFlags.HorizontalCenter)
+                _envText = ConvertItems.itemToEnviromentVar(_txt.Text)
+                'TextRenderer.DrawText()
+                graphicImage.DrawString(_envText, _txt.Font, _color, _screenPos, stringFormat)
+
+            Next
+
+            'I am drawing a oval around my text.
+            ' graphicImage.DrawArc(New Pen(Color.Red, 3), 90, 235, 150, 50, 0, 360)
+
+            'Save the new image to the response output stream.
+            bitMapImage.Save(_imgToMemoryStream, System.Drawing.Imaging.ImageFormat.Png)
+            'Clean house.
+            graphicImage.Dispose()
+            bitMapImage.Dispose()
+
+            Me.pbBackground.BackgroundImage = Image.FromStream(_imgToMemoryStream)
+            'Me.pbBackground.BackgroundImageLayout = ImageLayout.Stretch
+        End If
+
+    End Sub
 End Module
---
+++ createBG/regActions.vb
@@ -0,0 +1,288 @@
+﻿Imports Microsoft.Win32
+Imports System.Drawing
+
+Public Class _reg
+    Public Shared Sub createRootKey()
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.CreateSubKey("Software\bgM", RegistryKeyPermissionCheck.Default)
+        _regKey = Nothing
+    End Sub
+    Public Shared Sub setRegWallpaper(_bg As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("Wallpaper", _bg, RegistryValueKind.String)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getRegWallpaper() As String
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("Wallpaper", "").ToString
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Function getRegX() As String
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("X").ToString
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Function getRegY() As String
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("Y").ToString
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setRegX(_pX As Integer)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("X", _pX)
+
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Sub setRegY(_pY As Integer)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("Y", _pY)
+
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Sub setRegFont(_font As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("Font", _font, RegistryValueKind.String)
+
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getRegFont() As String
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("Font")
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Function getInputFields() As Integer
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", False)
+        Return _regKey.GetValue("InputFields")
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setInputFields(_n As Integer)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("InputFields", _n, RegistryValueKind.DWord)
+
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getItemText(_n As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("text" & _n)
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setItemText(_n As String, _txt As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("text" & _n, _txt, RegistryValueKind.String)
+        _regKey = Nothing
+    End Sub
+
+
+    Public Shared Sub delItemText(_n As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.DeleteValue("text" & _n, False)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getItemFont(_n As String) As font
+        Dim _font As Font
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Dim _fontstyle As FontStyle = _regKey.GetValue("text" & _n & "FontStyle", 0)
+        _font = New Font(_regKey.GetValue("text" & _n & "FontFamily", "Arial").ToString, Convert.ToSingle(_regKey.GetValue("text" & _n & "FontSize", "16")), _fontstyle)
+        Return _font
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setItemFont(_n As String, _font As Font)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("text" & _n & "FontStyle", _font.Style, RegistryValueKind.DWord)
+        _regKey.SetValue("text" & _n & "FontFamily", _font.FontFamily.Name, RegistryValueKind.String)
+        _regKey.SetValue("text" & _n & "FontSize", _font.Size, RegistryValueKind.DWord)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Sub delItemFont(_n As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.DeleteValue("text" & _n & "FontStyle", False)
+        _regKey.DeleteValue("text" & _n & "FontFamily", False)
+        _regKey.DeleteValue("text" & _n & "FontSize", False)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getItemLocation(_n As String) As String
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("text" & _n & "Location")
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setItemLocation(_n As String, _txtLocation As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("text" & _n & "Location", _txtLocation, RegistryValueKind.String)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Sub delItemLocation(_n As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.DeleteValue("text" & _n & "Location", False)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getItemColor(_n As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("text" & _n & "Color")
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setItemColor(_n As String, _intColor As Integer)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("text" & _n & "Color", _intColor, RegistryValueKind.DWord)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Sub delItemColor(_n As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.DeleteValue("text" & _n & "Color", False)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getItemWidth(_n As String) As Integer
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("text" & _n & "Width")
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setItemWidth(_n As String, _intWidth As Integer)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("text" & _n & "Width", _intWidth, RegistryValueKind.DWord)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Sub delItemWidth(_n As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.DeleteValue("text" & _n & "Width", False)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getItemAlign(_n As String) As Integer
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("text" & _n & "Align", 0)
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setItemAlign(_n As String, intAlign As Integer)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("text" & _n & "Align", intAlign, RegistryValueKind.DWord)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Sub delItemAlign(_n As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.DeleteValue("text" & _n & "Align", False)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getFontFamily() As String
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", False)
+        Return _regKey.GetValue("FontFamily", "Arial")
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setFontFamily(ByVal _font As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("FontFamily", _font, RegistryValueKind.String)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getFontSize() As String
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("FontSize", "10")
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setFontSize(ByVal _size As String)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("FontSize", _size, RegistryValueKind.String)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getFontStyle() As FontStyle
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("FontStyle", FontStyle.Regular)
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setFontStyle(ByVal _style As Integer)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("FontStyle", _style, RegistryValueKind.DWord)
+        _regKey = Nothing
+    End Sub
+
+    Public Shared Function getFontColor() As Integer
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        Return _regKey.GetValue("FontColor", 0)
+
+        _regKey = Nothing
+    End Function
+
+    Public Shared Sub setFontColor(ByVal _color As Integer)
+        Dim _regKey As RegistryKey
+        _regKey = Registry.CurrentUser.OpenSubKey("Software\bgM", True)
+        _regKey.SetValue("FontColor", _color, RegistryValueKind.DWord)
+        _regKey = Nothing
+    End Sub
+End Class
